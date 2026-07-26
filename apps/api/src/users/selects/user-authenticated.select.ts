d36b888818a8c89add14c68a2937be85ba529acc
Fernando Romero <fernando_0135@hotmail.com>
2026-07-26 20:28:11 -0300

    feat(auth): add user registration flow

--- apps/api/src/users/selects/user-authenticated.select.ts
+++ apps/api/src/users/selects/user-authenticated.select.ts
@@ -3,8 +3,11 @@ import { Prisma } from '@prisma/client';
 export const userAuthenticatedSelect = {
   id: true,
   email: true,
+  firstName: true,
+  lastName: true,
   role: true,
   isActive: true,
+  profileCompleted: true,
 } satisfies Prisma.UserSelect;
 
 export type UserAuthenticated = Prisma.UserGetPayload<{
